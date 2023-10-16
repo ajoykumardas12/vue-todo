1cf10b1a1640e64e93d57b93dcf12ff7acb5818a
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-10-16 21:51:21 +0530

    first component - header

---
+++ src/components/Todo-Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">Vue To-Do</header>
+</template>
+
+<script>
+export default {
+  name: 'Todo-Header'
+}
+</script>
+
+<style scoped>
+.header {
+  padding: 0.5rem 1rem;
+  background: #426ac6;
+  color: #ffffff;
+  font-size: 2rem;
+  font-weight: 600;
+}
+</style>
